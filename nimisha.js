fb67625da3201b0d155427d5b6a81e889b86cc83
Saurav Dahal <dahal.saurav444@gmail.com>
2022-09-18 23:07:35 +1000

    added console.log in nimisha file

--- nimisha.js
+++ nimisha.js
@@ -2,4 +2,9 @@ const num = [12,34,5,66,7]
 
 const filteredLessThan20 = num.filter((item)=>{
     return item< 20
+    console.log(item)
+    
 })
+filteredLessThan20();
+
+console.log(filteredLessThan20);
